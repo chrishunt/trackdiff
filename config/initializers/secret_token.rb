975445f423c3a208f1387c63c2244ec9ed33d671
Chris Hunt <huntca@gmail.com>
2011-08-23 14:08:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diffme::Application.config.secret_token = 'c0b3dd10b6466ec6c6c7fbc8e5b1a25b4cc1ae7648bf9b1797014446c5a75923c5df1b8e9370b309f74ba7ed75b78185e84a2ea25c99bd03f6d4caf70045c359'
