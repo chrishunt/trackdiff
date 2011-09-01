489c01afbe5487f71158b75f897615c6ae5d0642
Chris Hunt <huntca@gmail.com>
2011-09-01 11:12:46 -0700

    Added new site form

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,4 +2,8 @@ class SitesController < ApplicationController
   def index
     @sites = Site.all
   end
+
+  def new
+    @site = Site.new
+  end
 end
--- spec/requests/sites_spec.rb
+++ spec/requests/sites_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe "Sites" do
-  describe "GET /sites" do
-    before :each do
-      @user = User.create!(:email => "bob@email.com")
-    end
+  before :each do
+    @user = User.create!(:email => "bob@email.com")
+  end
 
+  describe "GET /sites" do
     it 'shows all site urls' do
       Site.create!(:url => "http://google.com", :user => @user)
       visit sites_path
       page.should have_content("http://google.com")
+    end
+  end
+
+  describe "GET /sites/new" do
+    it 'shows new site form' do
+      visit new_site_path
+      page.should have_content("URL")
+      page.should have_button("Track Diffs")
     end
   end
 end
